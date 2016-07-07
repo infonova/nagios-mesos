716b76bac59a66d1ab372433a61ea5b69d9f5fbf
Steven Schlansker <sschlansker@opentable.com>
2016-07-07 12:41:05 -0700

    0.2.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="nagios-mesos",
     description="A selection of Nagios plugins to monitor Apache Mesos.",
     long_description=open('README.rst').read(),
-    version="0.2.5",
+    version="0.2.6",
     packages=find_packages(),
     author='Steven Schlansker',
     author_email='sschlansker@opentable.com',
